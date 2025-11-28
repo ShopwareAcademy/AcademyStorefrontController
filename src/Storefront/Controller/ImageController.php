4853e43e51712e27d90a14e0650b17a7afe0adbe
Beytullah Cevik <b.cevik@shopware.com>
2025-11-28 10:16:48 +0100

    fix: small adaptations

--- src/Storefront/Controller/ImageController.php
+++ src/Storefront/Controller/ImageController.php
@@ -2,12 +2,17 @@
 
 namespace ShopwareAcademy\StorefrontController\Storefront\Controller;
 
-use Shopware\Core\System\SalesChannel\SalesChannelContext;
-use Shopware\Storefront\Controller\StorefrontController;
 use Symfony\Component\HttpFoundation\Response;
-use Shopware\Core\System\SystemConfig\SystemConfigService;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\DecodingExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Shopware\Core\System\SalesChannel\SalesChannelContext;
+use Shopware\Core\System\SystemConfig\SystemConfigService;
+use Shopware\Storefront\Controller\StorefrontController;
 
 #[Route(defaults: ['_routeScope' => ['storefront'], 'XmlHttpRequest' => true])]
 class ImageController extends StorefrontController
@@ -19,6 +24,13 @@ class ImageController extends StorefrontController
     {
     }
 
+    /**
+     * @throws TransportExceptionInterface
+     * @throws ServerExceptionInterface
+     * @throws RedirectionExceptionInterface
+     * @throws DecodingExceptionInterface
+     * @throws ClientExceptionInterface
+     */
     #[Route(
         path: '/image',
         name: 'frontend.image.show',
@@ -26,14 +38,19 @@ class ImageController extends StorefrontController
     )]
     public function showImage(SalesChannelContext $context): Response
     {
-        $apiAccessKey = $this->systemConfigService->get('AcademyStorefrontController.config.apiAccessKey', $context->getSalesChannelId());
-        $apiProvider = $this->systemConfigService->get('AcademyStorefrontController.config.apiProvider', $context->getSalesChannelId());
+        $pluginConfig = $this->systemConfigService->get('AcademyStorefrontController.config', $context->getSalesChannelId());
+        $apiAccessKey = $pluginConfig['apiAccessKey'];
+        $apiProvider = $pluginConfig['apiProvider'];
 
-        $response = $this->client->request('GET', 'https://api.'.$apiProvider.'.com/v1/images/search', [
-            'headers' => [
-                'x-api-key' => $apiAccessKey
+        $response = $this->client->request(
+            'GET',
+            'https://api.' . $apiProvider . '.com/v1/images/search',
+            [
+                'headers' => [
+                    'x-api-key' => $apiAccessKey
+                ]
             ]
-        ]);
+        );
 
         $data = $response->toArray();
         $imageUrl = $data[0]['url'];
